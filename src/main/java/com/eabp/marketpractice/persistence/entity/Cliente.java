ae141eacfdbb61a402f2f50a8144dfab60cd004a
Ezequiel Bermudez <ezequielandresbp@gmail.com>
2023-07-13 00:23:46 -0400

    feat (persistence): models added

---
+++ src/main/java/com/eabp/marketpractice/persistence/entity/Cliente.java
@@ -0,0 +1,68 @@
+package com.eabp.marketpractice.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private Long celular;
+    private String direccion;
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
